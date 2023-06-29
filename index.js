3d2271c1a3d28d1666559dc2afb3459479cf3df5
EzraMachoka <ezra.machoka@student.moringaschool.com>
2023-06-29 23:51:13 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+// code your solution hfunction saturdayFun(activity="roller-skate"){
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(action = "go to the office"){
+    return `This Monday, I will ${action}.`
+}
+function wrapAdjective(wraps ="*"){
+    return wrapAdjective
+    }
